b229832e7fcb20b2f547b4dcfdf3f67e97f8eade
Daaanxx <dannxko@gmail.com>
2023-04-03 15:37:36 +0100

    Added function to change time blocks colors

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Current day display var & function
-var currrentDay = $("#currentDay");
+var currentDay = $("#currentDay");
 
 function displayDay() {
   var todaysDay = moment().format("dddd, Do of MMMM ");
-  currrentDay.text(todaysDay);
+  currentDay.text(todaysDay);
+  // console.log(todaysDay); - Checking if var above returns current day - WORKS!
 }
-
 displayDay();
 
-// Each of a tibler block will habe one kay value pare to save on one time block
+// Time Blocks Function - colors for past/preset/future
+$(function () {
+  // To get CURRENT HOUR of the day
+  var currentTime = moment().format("H");
+  //  console.log(currentTime); - Checking if var above returns current time - WORKS!
+  // Function to change colors based on current time
+  function colors() {
+    $(".time-block").each(function () {
+      var Hour = parseInt(this.id);
+      $(this).toggleClass("past", Hour < currentTime);
+      $(this).toggleClass("present", Hour == currentTime);
+      $(this).toggleClass("future", Hour > currentTime);
+    });
+  }
+  // Calling above function
+  colors();
+});
